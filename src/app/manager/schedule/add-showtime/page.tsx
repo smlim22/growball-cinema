21cbd35f1ec38ff9ab26e66614dfed1bc6cf002c
smlim22 <limsm-sm22@student.tarc.edu.my>
2025-10-30 16:16:45 +0800

    Add showtime UI complete

--- src/app/manager/schedule/add-showtime/page.tsx
+++ src/app/manager/schedule/add-showtime/page.tsx
@@ -1,5 +1,10 @@
+"use client";
 import { Theme, Button } from '@radix-ui/themes';
 import { ArrowLeftIcon, PlusIcon } from "@radix-ui/react-icons";
+import Form from 'next/form';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { supabase } from '@/app/lib/supabaseClient';
 
 export default function AddShowtimePage(){
     return(
@@ -7,11 +12,42 @@ export default function AddShowtimePage(){
             <Theme className="inline">
                 <h1 className="text-2xl font-bold font-inter mb-4">Add Showtime</h1>
                 <div className="bg-white p-6 rounded-lg shadow-md">
-                    <a href="/manager/schedule" className="text-black hover:underline mb-4 flex gap-1 items-center">
+                    <a href="/manager/schedule" className="text-black hover:underline mb-4 flex gap-1 items-center font-inter">
                         <ArrowLeftIcon />
                         Back
                     </a>
                     <hr className="my-2 text-gray-300" />
+                    <Form action="/manager/schedule" className="grid grid-cols-2 space-y-4 gap-4 mt-6 font-inter">
+                        <div className='flex flex-col gap-1'>
+                            <label>Movie Name<span className="text-red-500">*</span></label>
+                            <select>
+                                <option>Select A Movie</option>
+                            </select>
+                        </div>
+                        <div className='flex flex-col gap-1'>
+                            <label>Cinema Hall<span className="text-red-500">*</span></label>
+                            <select>
+                                <option>Cinema Hall</option>
+                            </select>
+                        </div>
+                        <div className='flex flex-col gap-1'>
+                            <label>Date<span className="text-red-500">*</span></label>
+                            <input type="date"></input>
+                        </div>
+                        <div className='flex flex-col gap-1'>
+                            <label>Time<span className="text-red-500">*</span></label>
+                            <input type="time"></input>
+                        </div>
+                        
+                        <div></div>
+
+                        <div className="justify-self-end">
+                            <Button color="green" size="2" variant="solid" type="submit">
+                                <PlusIcon />
+                                Add F&B Item
+                            </Button>
+                        </div>
+                    </Form>
                 </div>
             </Theme>
         </div>
