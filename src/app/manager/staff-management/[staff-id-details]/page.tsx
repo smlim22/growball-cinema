a872f2a0677af17a5b31ad949a9499af9b7fc10f
smlim22 <limsm-sm22@student.tarc.edu.my>
2025-11-04 16:01:25 +0800

    Staff management and details now running

---
+++ src/app/manager/staff-management/[staff-id-details]/page.tsx
@@ -0,0 +1,92 @@
+'use client';
+import { Theme, Button, Callout, Flex } from '@radix-ui/themes';
+import { PlusIcon, Pencil2Icon, EyeOpenIcon, CheckCircledIcon, ArrowLeftIcon } from "@radix-ui/react-icons";
+import { useEffect, useState } from 'react';
+import { supabase } from "@/app/lib/supabaseClient";
+import { useParams, useRouter, useSearchParams } from "next/navigation";
+
+type Staff = {
+    staff_id: number,
+    staff_name: string,
+    staff_email: string,
+    staff_phoneNo: string,
+    access_level: number,
+    joined_at: string
+}
+
+export default function StaffDetailsPage() {
+    const params = useParams();
+    const staffId = params['staff-id-details'];
+    const [staffDetails, setStaffDetails] = useState<Staff | null>(null);
+
+    useEffect(() => {
+        const fetchStaffDetails = async () => {
+            if(!staffId){
+                console.warn("No Staff ID found in params:", params);
+                return
+            }
+
+            let query = supabase.from("staff").select("*").eq("staff_id", Number(staffId)).single();
+            
+            const { data, error } = await query;
+
+            if (error) {
+                console.error("Error fetching staff details:", error);
+            } else {
+                setStaffDetails(data ?? null);
+            }
+        }
+
+        fetchStaffDetails();
+    }, [staffId])
+
+    if (!staffDetails){
+        return(
+            <Theme className="inline">
+                <Callout.Root color="red" size="2" variant="soft" className="font-inter mx-12 my-10">
+                <Callout.Text className='font-inter'>Staff not found.</Callout.Text>
+                </Callout.Root>
+            </Theme>
+        )
+    }
+
+    return(
+        <div className="py-10 px-12">
+            <h1 className="text-2xl font-bold mb-4">Staff Details</h1>
+            <div className="bg-white p-6 rounded-lg shadow-md space-y-3">
+                <a
+                    href="/manager/movies"
+                    className="text-black hover:underline flex gap-1 items-center"
+                >
+                    <ArrowLeftIcon />
+                    Back
+                </a>
+                <hr className="my-2 text-gray-300" />
+                <table className="min-w-full border border-collapse border-gray-200 rounded-md font-inter my-4">
+                    <tbody>
+                        <tr>
+                            <td className="border border-gray-200 py-3 px-4 font-medium bg-gray-50 w-1/3">Staff Name</td>
+                            <td className="border border-gray-200 py-3 px-4">{staffDetails.staff_name}</td>
+                        </tr>
+                        <tr>
+                            <td className="border border-gray-200 py-3 px-4 font-medium bg-gray-50">Email Address</td>
+                            <td className="border border-gray-200 py-3 px-4">{staffDetails.staff_email}</td>
+                        </tr>
+                        <tr>
+                            <td className="border border-gray-200 py-3 px-4 font-medium bg-gray-50">Phone No</td>
+                            <td className="border border-gray-200 py-3 px-4">{staffDetails.staff_phoneNo}</td>
+                        </tr>
+                        <tr>
+                            <td className="border border-gray-200 py-3 px-4 font-medium bg-gray-50">Role</td>
+                            <td className="border border-gray-200 py-3 px-4">{staffDetails.access_level === 1 ? `Staff` : staffDetails.access_level === 2 ? `Manager` : ``}</td>
+                        </tr>
+                        <tr>
+                            <td className="border border-gray-200 py-3 px-4 font-medium bg-gray-50">Role</td>
+                            <td className="border border-gray-200 py-3 px-4">{staffDetails.joined_at}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
--- src/app/manager/staff-management/page.tsx
+++ src/app/manager/staff-management/page.tsx
@@ -1,14 +1,46 @@
 'use client';
-import { Theme, Button, Callout } from '@radix-ui/themes';
-import { PlusIcon, Pencil2Icon, CheckCircledIcon } from "@radix-ui/react-icons";
-import { useEffect } from 'react';
+import { Theme, Button, Callout, Flex } from '@radix-ui/themes';
+import { PlusIcon, Pencil2Icon, EyeOpenIcon, CheckCircledIcon } from "@radix-ui/react-icons";
+import { useEffect, useState } from 'react';
+import { supabase } from "@/app/lib/supabaseClient";
+import { useRouter, useSearchParams } from "next/navigation";
+
+type Staff = {
+    staff_id: number,
+    staff_name: string,
+    staff_email: string,
+    access_level: number,
+}
 
 export default function StaffManagementPage(){
+    const [staff, setStaff] = useState<Staff[]>([]);
+    const router = useRouter();
+
+    const searchParams = useSearchParams();
+    const success = searchParams.get('success');
+    const updated = searchParams.get('updated');
+
+    useEffect(() => {
+        const fetchStaff = async () => {
+            let query = supabase.from("staff").select("*");
+
+            const { data, error } = await query;
+
+            if(error){
+                console.error("Error fetching list of staff", error);
+            }else{
+                setStaff(data ?? []);
+            }
+        }
+
+        fetchStaff();
+    })
+
     return (
         <div className="py-10 px-12">
             <Theme className="inline">
                 <div className="flex items-center justify-between mb-4 font-inter">
-                    <h1 className="text-2xl font-bold font-inter">Schedule</h1>
+                    <h1 className="text-2xl font-bold font-inter">Staff Management</h1>
                     <Button
                         color="green"
                         size="2"
@@ -19,6 +51,19 @@ export default function StaffManagementPage(){
                         Add New Staff
                     </Button>
                 </div>
+                {success && (
+                    <Callout.Root color="green" size="2" variant="soft" className="mb-4">
+                        <Callout.Icon><CheckCircledIcon /></Callout.Icon>
+                        <Callout.Text className='font-inter'>Showtime added successfully!</Callout.Text>
+                    </Callout.Root>
+                )}
+        
+                {updated && (
+                    <Callout.Root color="green" size="2" variant="soft" className="mb-4">
+                        <Callout.Icon><CheckCircledIcon /></Callout.Icon>
+                        <Callout.Text className='font-inter'>Showtime updated!</Callout.Text>
+                    </Callout.Root>
+                )}
                 <table className="min-w-full bg-white shadow-md rounded-lg border-collapse border overflow-hidden font-inter">
                     <thead className="bg-signature-red text-white">
                         <tr>
@@ -30,7 +75,43 @@ export default function StaffManagementPage(){
                         </tr>
                     </thead>
                     <tbody>
-
+                        {staff.length > 0 ? (
+                            staff.map((s, index ) =>(
+                                <tr key={s.staff_id} className="border-t border-gray-200 hover:bg-gray-50">
+                                    <td className="py-3 px-6">{index+1}</td>
+                                    <td className="py-3 px-6">{s.staff_name}</td>
+                                    <td className="py-3 px-6">{s.staff_email}</td>
+                                    <td className="py-3 px-6">{s.access_level === 1 ? `Staff` : s.access_level === 2 ? `Manager` : ``}</td>
+                                    <td className="py-3 px-6">
+                                        <Flex gap="2">
+                                            <Button
+                                                color="blue"
+                                                size="2"
+                                                variant="solid"
+                                                onClick={() => router.push(`/manager/staff-management/${s.staff_id}`)}
+                                            >
+                                                <EyeOpenIcon />
+                                                View
+                                            </Button>
+                                            <Button
+                                                color="amber"
+                                                size="2"
+                                                variant="solid"
+                                            >
+                                                <Pencil2Icon />
+                                                Edit
+                                            </Button>
+                                        </Flex>
+                                    </td>
+                                </tr>
+                            ))
+                        ) : (
+                            <tr>
+                                <td className="py-3 px-6 text-center" colSpan={5}>
+                                    No staff found.
+                                </td>
+                            </tr>
+                        )}
                     </tbody>
                 </table>
             </Theme>
