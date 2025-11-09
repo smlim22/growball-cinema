7cb43f9c6d6a4e68e2ca7bb69a0189100b8d71d7
smlim22 <limsm-sm22@student.tarc.edu.my>
2025-11-09 13:53:30 +0800

    Too hot to do

--- src/app/staff/page.tsx
+++ src/app/staff/page.tsx
@@ -1,8 +1,53 @@
+'use client'
+import { supabase } from "@/app/lib/supabaseClient";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { MoveRight } from "lucide-react";
+
 export default function StaffPage() {
+  const router = useRouter();
+  const [name, setName] = useState('');
+  const today = new Date().toISOString().split("T")[0];
+
+  useEffect(() => {
+    const getUserName = async () => {
+      const {data: {user}} = await supabase.auth.getUser();
+
+      if (!user){
+        router.replace("/");
+        return;
+      }
+
+      const { data, error } = await supabase
+        .from("staff")
+        .select("staff_name")
+        .eq("uuid", user.id)
+        .single();
+
+      if (error) {
+        console.error("Error fetching staff name:", error);
+        return;
+      }
+
+      setName(data?.staff_name);
+    }
+    getUserName();
+  });
+
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded-2xl shadow-md w-full max-w-md text-center">
-        <h1 className="text-2xl font-bold mb-4">Staff Dashboard</h1>
+    <div className="py-10 px-12 font-inter">
+      <h1 className="text-2xl font-bold mb-4">Welcome, {name}</h1>
+      <div className="flex gap-4">
+        <div className="inline-block bg-white p-3 rounded-md shadow-md">
+          <h2 className="text-xl font-semibold my-4">Start Transaction</h2>
+          <p className="text-lg mb-4 justify-items-end">
+            <MoveRight />
+          </p>
+        </div>
+        <div className="inline-block bg-white p-3 rounded-md shadow-md">
+          <h2 className="text-xl font-semibold my-4">Orders Pending</h2>
+          <p className="text-lg mb-4">0</p>
+        </div>
       </div>
     </div>
   )
---
+++ src/app/staff/pos/page.tsx
@@ -0,0 +1,12 @@
+import { Theme, Button, Callout, Flex } from '@radix-ui/themes';
+
+export default function POSPage() {
+    return (
+        <div className="py-10 px-12 ">
+            <Theme className='inline'>
+                <h1 className='text-2xl font-bold font-inter mb-4'>Point Of Sales</h1>
+                <div className='bg-white rounded-lg shadow-md p-4'> hello</div>
+            </Theme>
+        </div>
+    )
+}
