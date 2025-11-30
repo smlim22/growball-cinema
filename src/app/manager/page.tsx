f17fa8cd73a702b59fb08a200f94596edad578c1
smlim22 <limsm-sm22@student.tarc.edu.my>
2025-11-30 16:28:59 +0800

    Manager dashboard UI updated

--- src/app/manager/page.tsx
+++ src/app/manager/page.tsx
@@ -2,6 +2,8 @@
 import { supabase } from "@/app/lib/supabaseClient";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import { Theme, Button } from '@radix-ui/themes';
+import { FileIcon } from "@radix-ui/react-icons";
 
 export default function ManagerPage() {
   const router = useRouter();
@@ -54,7 +56,7 @@ export default function ManagerPage() {
   return (
     <div className="py-10 px-12 font-inter">
       <h1 className="text-2xl font-bold mb-4">Welcome, {name}</h1>
-      <div className="flex gap-4">
+      <div className="flex gap-4 mb-8">
         <div className="inline-block bg-white p-3 rounded-md shadow-md">
           <h2 className="text-xl font-semibold my-4">Today's Running Showtime</h2>
           <p className="text-lg mb-4">{noOfShowtimes}</p>
@@ -62,6 +64,63 @@ export default function ManagerPage() {
         <div className="inline-block bg-white p-3 rounded-md shadow-md">
           <h2 className="text-xl font-semibold my-4">Complaints Reported Today</h2>
           <p className="text-lg mb-4">0</p>
+        </div>
+      </div>
+      <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
+      <div className="flex gap-4 flex-row">
+        <div className="bg-white p-4 rounded-md shadow-md">
+          <div className="flex items-center gap-4 mb-4">
+            <h2 className="text-xl font-bold">Ticket Sales Summary</h2>
+            <div className="flex gap-4">
+              <div className="flex gap-2 items-center">
+                <label>Year:</label>
+                <select className="border border-gray-300 p-2 rounded-md">
+                  <option>2025</option>
+                </select>
+              </div>
+              <div className="flex gap-2 items-center">
+                <label>Month:</label>
+                <select className="border border-gray-300 p-2 rounded-md">
+                  <option>January</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <Theme className="inline">
+            <Button
+              variant="solid"
+              color="green"
+            >
+              <FileIcon/> Download Detailed Report
+            </Button>
+          </Theme>
+        </div>
+        <div className="bg-white p-4 rounded-md shadow-md">
+          <div className="flex items-center gap-4 mb-4">
+            <h2 className="text-xl font-bold">FNB Sales Summary</h2>
+            <div className="flex gap-4">
+              <div className="flex gap-2 items-center">
+                <label>Year:</label>
+                <select className="border border-gray-300 p-2 rounded-md">
+                  <option>2025</option>
+                </select>
+              </div>
+              <div className="flex gap-2 items-center">
+                <label>Month:</label>
+                <select className="border border-gray-300 p-2 rounded-md">
+                  <option>January</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <Theme className="inline">
+            <Button
+              variant="solid"
+              color="green"
+            >
+              <FileIcon/> Download Detailed Report
+            </Button>
+          </Theme>
         </div>
       </div>
     </div>
